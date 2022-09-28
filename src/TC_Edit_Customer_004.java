87522fcfb21097e749229e2e2b976821238f92a8
Nyunwu <tumbam25@gmail.com>
2022-09-28 08:17:47 -0400

    Added search_customer file and modify edit customer file

--- src/TC_Edit_Customer_004.java
+++ src/TC_Edit_Customer_004.java
@@ -1,2 +1,8 @@
 //This is a new Test case
 //This test will check editing a new customer
+
+//This is code added newly
+//This is code added newly
+//This is code added newly
+//This is code added newly
+//This is code added newly
---
+++ src/TC_Search_Customer_005.java
@@ -0,0 +1,11 @@
+
+public class TC_Search_Customer_005 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//This is my search customer test case
+
+	}
+
+}
