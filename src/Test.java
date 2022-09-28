44dd74aed3aebb58331bd8e74f2610f20e34b5bd
Nyunwu <tumbam25@gmail.com>
2022-09-27 22:51:52 -0400

    First round of commit...

---
+++ src/Test.java
@@ -0,0 +1,16 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String hungry = "Yes";
+		
+		if(hungry=="Yes") {
+			System.out.println("Eat something.....");
+		}else {
+			System.out.println("Do work.......");
+		}
+	}
+
+}
